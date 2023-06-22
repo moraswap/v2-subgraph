27a7566d263f8c5e4d71ca75e7666c234f29e574
0xkaidev <0xkai.dev@gmail.com>
2023-06-22 16:31:11 +0700

    init

---
+++ src/constants/index.template.ts
@@ -0,0 +1,13 @@
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+export const FACTORY_ADDRESS = '{{factoryv2_address}}'
+
+export const WETH9_ADDRESS = '{{weth9_address}}'
+export const DAI_ADDRESS = '{{dai_address}}' // DAI
+export const USDC_ADDRESS = '{{usdc_address}}' // USDC
+export const USDT_ADDRESS = '{{usdt_address}}' // USDT
+export const WBTC_ADDRESS = '{{wbtc_address}}' // WBTC
+export const WSOL_ADDRESS = '{{wsol_address}}' // WSOL
+
+export const USDC_WETH9_PAIR = '{{usdc_weth9_pair_address}}' // created at block {{usdc_weth9_pair_create_block}}
+export const DAI_WETH9_PAIR = '{{dai_weth9_pair_address}}' // created block {{dai_weth9_pair_create_block}}
+export const USDT_WETH9_PAIR = '{{usdt_weth9_pair_address}}' // created block {{usdt_weth9_pair_create_block}}
